30b51e378d7ddd02401107f7cc41255d13d99c6a
carabrook <cbrook04@gmail.com>
2023-04-26 05:13:45 -0500

    updates to fig 1

--- figure-2/Fig2_S2_S3.R
+++ figure-2/Fig2_S2_S3.R
@@ -22,28 +22,52 @@ par.plots <- function(dat.var, tol.shape, sub){
   # and add labels for strip labels
   dat.var$label= NA
   
-  dat.var$label[dat.var$variable=="c"] <- "c~', virus consumption'"
-  dat.var$label[dat.var$variable=="g"] <- "g~', leukocyte activation'"
-  dat.var$label[dat.var$variable=="m"] <- "m~', leukocyte mortality'"
-  dat.var$label[dat.var$variable=="mu"] <- "mu~', host background mortality'"
-  dat.var$label[dat.var$variable=="g0"] <- "g[0]~', constitutive immunity'"
+  dat.var$label[dat.var$variable=="c"] <- "atop(c[R]~', virus consumption', 'rate in reservoir host')"
+  dat.var$label[dat.var$variable=="g"] <- "atop(g[R]~', reservoir', 'leukocyte activation rate')"
+  dat.var$label[dat.var$variable=="m"] <- "atop(m[R]~', reservoir', 'leukocyte mortality rate')"
+  dat.var$label[dat.var$variable=="mu"] <- "atop(mu[R]~', reservoir host', 'background mortality rate')"
+  dat.var$label[dat.var$variable=="g0"] <- "atop(g[0~R]~', magnitude reservoir', 'constitutive immunity')"
   #dat.var$label[dat.var$variable=="Tv_spill"] <- "T[vS]~', spillover host tolerance of direct virus pathology'"
   #dat.var$label[dat.var$variable=="Tw_spill"] <- "T[wS]~', spillover host tolerance of immunopathology'"
   
   
-  dat.var$label = factor(dat.var$label, levels = c("mu~', host background mortality'",
-                                                   "g[0]~', constitutive immunity'",
-                                                   "g~', leukocyte activation'",
-                                                   "c~', virus consumption'",
-                                                   "m~', leukocyte mortality'"))#,
+  dat.var$label = factor(dat.var$label, levels = c("atop(mu[R]~', reservoir host', 'background mortality rate')",
+                                                   "atop(g[0~R]~', magnitude reservoir', 'constitutive immunity')",
+                                                   "atop(g[R]~', reservoir', 'leukocyte activation rate')",
+                                                   "atop(c[R]~', virus consumption', 'rate in reservoir host')",
+                                                   "atop(m[R]~', reservoir', 'leukocyte mortality rate')"))#,
                                                    #"T[vS]~', spillover host tolerance of direct virus pathology'",
                                                    #"T[wS]~', spillover host tolerance of immunopathology'"))
   
   dat.var = subset(dat.var, tolerance_shape == tol.shape)
   if(tol.shape=="constant-tolerance"){
-    dat.var$tolerance = factor(dat.var$tolerance, levels=c("Tw=1.9", "Tv=1.9", "Tw=1.5", "Tv=1.5","Tw=1.1", "Tv=1.1"))
+    dat.var$tolerance <- as.character(dat.var$tolerance)
+    dat.var$tolerance[dat.var$tolerance=="Tw=1.9"] <- bquote('T[wR]==1.9')
+    dat.var$tolerance[dat.var$tolerance=="Tv=1.9"] <- bquote('T[vR]==1.9')
+    dat.var$tolerance[dat.var$tolerance=="Tw=1.5"] <- bquote('T[wR]==1.5')
+    dat.var$tolerance[dat.var$tolerance=="Tv=1.5"] <- bquote('T[vR]==1.5')
+    dat.var$tolerance[dat.var$tolerance=="Tw=1.1"] <- bquote('T[wR]==1.1')
+    dat.var$tolerance[dat.var$tolerance=="Tv=1.1"] <- bquote('T[vR]==1.1')
+    
+    dat.var$tolerance = factor(dat.var$tolerance, levels=c(bquote('T[wR]==1.9'), bquote('T[vR]==1.9'), bquote('T[wR]==1.5'), bquote('T[vR]==1.5'),bquote('T[wR]==1.1'), bquote('T[vR]==1.1')))
+    
+    
+    
+    #dat.var$tolerance = factor(dat.var$tolerance, levels=c("Tw=1.9", "Tv=1.9", "Tw=1.5", "Tv=1.5","Tw=1.1", "Tv=1.1"))
   }else{
-    dat.var$tolerance = factor(dat.var$tolerance, levels=c("Tw=0.9", "Tv=0.9", "Tw=0.5", "Tv=0.5","Tw=0.2", "Tv=0.2"))
+    
+    dat.var$tolerance <- as.character(dat.var$tolerance)
+    dat.var$tolerance[dat.var$tolerance=="Tw=0.9"] <- bquote('T[wR]==0.9')
+    dat.var$tolerance[dat.var$tolerance=="Tv=0.9"] <- bquote('T[vR]==0.9')
+    dat.var$tolerance[dat.var$tolerance=="Tw=0.5"] <- bquote('T[wR]==0.5')
+    dat.var$tolerance[dat.var$tolerance=="Tv=0.5"] <- bquote('T[vR]==0.5')
+    dat.var$tolerance[dat.var$tolerance=="Tw=0.2"] <- bquote('T[wR]==0.2')
+    dat.var$tolerance[dat.var$tolerance=="Tv=0.2"] <- bquote('T[vR]==0.2')
+    
+    dat.var$tolerance = factor(dat.var$tolerance, levels=c(bquote('T[wR]==0.9'), bquote('T[vR]==0.9'), bquote('T[wR]==0.5'), bquote('T[vR]==0.5'),bquote('T[wR]==0.2'), bquote('T[vR]==0.2')))
+    
+    
+    #dat.var$tolerance = factor(dat.var$tolerance, levels=c("Tw=0.9", "Tv=0.9", "Tw=0.5", "Tv=0.5","Tw=0.2", "Tv=0.2"))
   }
   
   #dat.var = subset(dat.var, variable_par <=1)
@@ -62,22 +86,48 @@ par.plots <- function(dat.var, tol.shape, sub){
   
   if (tol.shape=="constant-tolerance"){
     
-    colz = c("Tw=1.9" = "dodgerblue4", 
-             "Tv=1.9" = "firebrick4",
-             "Tw=1.5"= "dodgerblue3", 
-             "Tv=1.5" = "firebrick3",  
-             "Tw=1.1" = "dodgerblue1",
-             "Tv=1.1" ="firebrick1")
+    
+    colz = c('T[wR]==1.9' = "dodgerblue4", 
+             'T[vR]==1.9' = "firebrick4",
+             'T[wR]==1.5'= "dodgerblue3", 
+             'T[vR]==1.5' = "firebrick3",  
+             'T[wR]==1.1' = "dodgerblue1",
+             'T[vR]==1.1' ="firebrick1")
+    
+    
+    #colz = c(bquote('T[w]==1.9') = "dodgerblue4", 
+     #        bquote('T[v]==1.9') = "firebrick4",
+      #       bquote('T[w]==1.5')= "dodgerblue3", 
+       #      bquote('T[v]==1.5') = "firebrick3",  
+        #     bquote('T[w]==1.1') = "dodgerblue1",
+         #    bquote('T[v]==1.1') ="firebrick1")
+    
+    
+    
+    # 
+    # colz = c("Tw=1.9" = "dodgerblue4", 
+    #          "Tv=1.9" = "firebrick4",
+    #          "Tw=1.5"= "dodgerblue3", 
+    #          "Tv=1.5" = "firebrick3",  
+    #          "Tw=1.1" = "dodgerblue1",
+    #          "Tv=1.1" ="firebrick1")
     
   }else if (tol.shape=="complete-tolerance"){
     
     
-    colz = c("Tw=0.9"  = "dodgerblue4",
-             "Tv=0.9"=  "firebrick4",
-             "Tw=0.5"= "dodgerblue3",
-             "Tv=0.5"=  "firebrick3",
-             "Tw=0.2"= "dodgerblue1", 
-             "Tv=0.2"=  "firebrick1")
+    #colz = c("Tw=0.9"  = "dodgerblue4",
+     #        "Tv=0.9"=  "firebrick4",
+      #       "Tw=0.5"= "dodgerblue3",
+       #      "Tv=0.5"=  "firebrick3",
+        #     "Tw=0.2"= "dodgerblue1", 
+         #    "Tv=0.2"=  "firebrick1")
+    
+    colz = c('T[wR]==0.9' = "dodgerblue4", 
+             'T[vR]==0.9' = "firebrick4",
+             'T[wR]==0.5'= "dodgerblue3", 
+             'T[vR]==0.5' = "firebrick3",  
+             'T[wR]==0.2' = "dodgerblue1",
+             'T[vR]==0.2' ="firebrick1")
     
   }
   
@@ -87,19 +137,19 @@ par.plots <- function(dat.var, tol.shape, sub){
   pA <- ggplot(data=r.dat) +
     geom_line(aes(x=variable_par, y=value, color=tolerance)) +
     facet_grid(~label, scales="free", labeller = label_parsed) +
-    ylab(bquote("r"^"*"~", virus growth")) + theme_bw() +
+    ylab(bquote(r[R]^"*"~", virus growth")) + theme_bw() +
     theme(panel.grid = element_blank(), axis.title.x = element_blank(), strip.text = element_text(size=15),
           axis.title.y = element_text(size=18), strip.background = element_rect(fill="white"), axis.text.y = element_text(size=14),
           axis.text.x = element_blank(),axis.ticks.x = element_blank(), plot.margin = unit(c(.5,.2,0,1.3), "cm"),
-          legend.position = c(.1,.2), legend.title = element_blank(), legend.text = element_text(size=10)) + 
-          scale_color_manual(values=colz) + guides(color=guide_legend(nrow=2)) + coord_cartesian(ylim = c(0,5))  
+          legend.position = c(.1,.2), legend.title = element_blank(), legend.text = element_text(size=8)) + 
+          scale_color_manual(values=colz, labels = function(x) parse(text = x)) + guides(color=guide_legend(nrow=2)) + coord_cartesian(ylim = c(0,5))  
  #  print(pA)
   
   
   pB <- ggplot(data=beta.dat) +
     geom_line(aes(x=variable_par, y=value, color=tolerance), show.legend = F) +
     facet_grid(~label, scales="free", labeller = label_parsed) +
-    ylab(bquote(beta^"*"~", transmission")) + theme_bw() +
+    ylab(bquote(beta[r[R]]^"*"~", transmission")) + theme_bw() +
     theme(panel.grid = element_blank(), axis.title.x = element_blank(), strip.text = element_blank(),
           axis.title.y = element_text(size=18), strip.background = element_blank(), axis.text.y = element_text(size=14),
           axis.text.x = element_blank(),axis.ticks.x = element_blank(), plot.margin = unit(c(0,.2,0,.4), "cm")) + coord_cartesian(ylim = c(0,.005)) +
@@ -113,7 +163,7 @@ par.plots <- function(dat.var, tol.shape, sub){
   pC <- ggplot(data=a.dat) +
     geom_line(aes(x=variable_par, y=value, color=tolerance), show.legend = F) +
     facet_grid(~label, scales="free", labeller = label_parsed) +
-    ylab(bquote(alpha^"*"~", virulence")) + theme_bw() +
+    ylab(bquote(alpha[r[R]]^"*"~", virulence")) + theme_bw() +
     theme(panel.grid = element_blank(), axis.title.x = element_blank(), strip.text = element_blank(),
           axis.title.y = element_text(size=18), strip.background = element_blank(), axis.text.y = element_text(size=14),
           axis.text.x = element_blank(), axis.ticks.x = element_blank(),
@@ -163,10 +213,10 @@ heat.plots <- function(dat.tol, tol.shape){
   
   
   pA <- ggplot(r.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
-        scale_fill_gradient(low="yellow", high="red", name=bquote('r'^'*')) +
+        scale_fill_gradient(low="yellow", high="red", name=bquote(r[R]^'*')) +
         theme_bw() + 
         # xlab(bquote('T'[w]~'tolerance of immunopathology'))+
-        ylab(bquote('T'[v]~'tolerance of viral pathology'))+
+        ylab(bquote('T'[vR]~'tolerance of viral pathology'))+
         theme(panel.grid = element_blank(),
               axis.title = element_blank(),
           axis.text.y = element_text(size=14, color="black"),
@@ -178,7 +228,7 @@ heat.plots <- function(dat.tol, tol.shape){
   
   
   pB <- ggplot(beta.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
-    scale_fill_gradient(low="yellow", high="red", name=bquote(beta^'*')) +
+    scale_fill_gradient(low="yellow", high="red", name=bquote(beta[r[R]]^'*')) +
     theme_bw() + 
     # xlab(bquote('T'[w]~'tolerance of immunopathology'))+
     # ylab(bquote('T'[v]~'tolerance of viral pathology'))+
@@ -190,9 +240,9 @@ heat.plots <- function(dat.tol, tol.shape){
           plot.margin = unit(c(0,.3,0,1), "cm"))
   
   pC <- ggplot(a.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
-    scale_fill_gradient(low="yellow", high="red", name=bquote(alpha^'*')) +
+    scale_fill_gradient(low="yellow", high="red", name=bquote(alpha[r[R]]^'*')) +
     theme_bw() + 
-     xlab(bquote('T'[w]~'tolerance of immunopathology'))+
+     xlab(bquote('T'[wR]~', tolerance of immunopathology'))+
     # ylab(bquote('T'[v]~'tolerance of viral pathology'))+
     theme(panel.grid = element_blank(),
           axis.title = element_blank(),
@@ -206,7 +256,7 @@ heat.plots <- function(dat.tol, tol.shape){
   pD <- ggplot(beta.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
     scale_fill_gradient(low="yellow", high="red", name=bquote(alpha[s])) +
     theme_bw() + 
-    xlab(bquote('T'[w]~'tolerance of immunopathology'))+
+    xlab(bquote('T'[wR]~', tolerance of immunopathology'))+
     # ylab(bquote('T'[v]~'tolerance of viral pathology'))+
     theme(panel.grid = element_blank(),
           axis.title.y = element_blank(),
@@ -225,7 +275,7 @@ heat.plots <- function(dat.tol, tol.shape){
   
   p.heat2 <- p.heat + 
              annotate("text", color="firebrick",
-                      label=bquote("T"[v]~", tolerance of virus pathology"), 
+                      label=bquote("T"[vR]~", tolerance of virus pathology"), 
                       x=.05,y=.55, angle=90, size=6)
              
  
@@ -288,10 +338,10 @@ heat.plots.complete <- function(dat.tol, tol.shape){
   
   
   pA <- ggplot(r.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
-    scale_fill_gradient(low="yellow", high="red", name=bquote('r'^'*'), trans="log10") +
+    scale_fill_gradient(low="yellow", high="red", name=bquote(r[R]^'*'), trans="log10") +
     theme_bw() + 
-    xlab(bquote('T'[w]~'tolerance of immunopathology'))+
-    ylab(bquote('T'[v]~'tolerance of viral pathology'))+
+    xlab(bquote('T'[wR]~', tolerance of immunopathology'))+
+    ylab(bquote('T'[vR]~', tolerance of viral pathology'))+
     theme(panel.grid = element_blank(),
           axis.title = element_blank(),
           axis.text.y = element_text(size=14, color="black"),
@@ -302,7 +352,7 @@ heat.plots.complete <- function(dat.tol, tol.shape){
   
   
   pB <- ggplot(beta.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
-    scale_fill_gradient(low="yellow", high="red", name=bquote(beta^'*'), trans="log10") +
+    scale_fill_gradient(low="yellow", high="red", name=bquote(beta[r[R]]^'*'), trans="log10") +
     theme_bw() + 
     # xlab(bquote('T'[w]~'tolerance of immunopathology'))+
     # ylab(bquote('T'[v]~'tolerance of viral pathology'))+
@@ -316,9 +366,9 @@ heat.plots.complete <- function(dat.tol, tol.shape){
   
   
   pC <- ggplot(a.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
-    scale_fill_gradient(low="yellow", high="red", name=bquote(alpha^'*')) +
+    scale_fill_gradient(low="yellow", high="red", name=bquote(alpha[r[R]]^'*')) +
     theme_bw() + 
-    xlab(bquote('T'[w]~'tolerance of immunopathology'))+
+    xlab(bquote('T'[wR]~', tolerance of immunopathology'))+
     # ylab(bquote('T'[v]~'tolerance of viral pathology'))+
     theme(panel.grid = element_blank(),
           axis.title = element_blank(),
@@ -332,7 +382,7 @@ heat.plots.complete <- function(dat.tol, tol.shape){
   pD <- ggplot(beta.dat) + geom_tile(aes(x=Tw, y=Tv, fill=value)) + 
     scale_fill_gradient(low="yellow", high="red", name=bquote(alpha[s]), trans="log10") +
     theme_bw() + 
-    xlab(bquote('T'[w]~'tolerance of immunopathology'))+
+    xlab(bquote('T'[wR]~', tolerance of immunopathology'))+
     # ylab(bquote('T'[v]~'tolerance of viral pathology'))+
     theme(panel.grid = element_blank(),
           axis.title.y = element_blank(),
@@ -351,7 +401,7 @@ heat.plots.complete <- function(dat.tol, tol.shape){
   
   p.heat2 <- p.heat + 
     annotate("text", color="firebrick",
-             label=bquote("T"[v]~", tolerance of virus pathology"), 
+             label=bquote("T"[vR]~", tolerance of virus pathology"), 
              x=.05,y=.55, angle=90, size=6)
   
   
@@ -359,21 +409,23 @@ heat.plots.complete <- function(dat.tol, tol.shape){
 }
 par.plots.complete <- function(dat.var, tol.shape, sub, plot.prev){
   
-  # and add labels for strip labels
-  dat.var$label= NA
   
-  dat.var$label[dat.var$variable=="c"] <- "c~', virus consumption'"
-  dat.var$label[dat.var$variable=="g"] <- "g~', leukocyte activation'"
-  dat.var$label[dat.var$variable=="m"] <- "m~', leukocyte mortality'"
-  dat.var$label[dat.var$variable=="mu"] <- "mu~', host background mortality'"
-  dat.var$label[dat.var$variable=="g0"] <- "g[0]~', constitutive immunity'"
+  dat.var$label[dat.var$variable=="c"] <- "atop(c[R]~', virus consumption', 'rate in reservoir host')"
+  dat.var$label[dat.var$variable=="g"] <- "atop(g[R]~', reservoir', 'leukocyte activation rate')"
+  dat.var$label[dat.var$variable=="m"] <- "atop(m[R]~', reservoir', 'leukocyte mortality rate')"
+  dat.var$label[dat.var$variable=="mu"] <- "atop(mu[R]~', reservoir host', 'background mortality rate')"
+  dat.var$label[dat.var$variable=="g0"] <- "atop(g[0~R]~', magnitude reservoir', 'constitutive immunity')"
+  #dat.var$label[dat.var$variable=="Tv_spill"] <- "T[vS]~', spillover host tolerance of direct virus pathology'"
+  #dat.var$label[dat.var$variable=="Tw_spill"] <- "T[wS]~', spillover host tolerance of immunopathology'"
   
   
-  dat.var$label = factor(dat.var$label, levels = c("mu~', host background mortality'",
-                                                   "g[0]~', constitutive immunity'",
-                                                   "g~', leukocyte activation'",
-                                                   "c~', virus consumption'",
-                                                   "m~', leukocyte mortality'"))
+  dat.var$label = factor(dat.var$label, levels = c("atop(mu[R]~', reservoir host', 'background mortality rate')",
+                                                   "atop(g[0~R]~', magnitude reservoir', 'constitutive immunity')",
+                                                   "atop(g[R]~', reservoir', 'leukocyte activation rate')",
+                                                   "atop(c[R]~', virus consumption', 'rate in reservoir host')",
+                                                   "atop(m[R]~', reservoir', 'leukocyte mortality rate')"))#,
+  #"T[vS]~', spillover host tolerance of direct virus pathology'",
+  #"T[wS]~', spillover host tolerance of immunopathology'"))
   
   dat.var = subset(dat.var, tolerance_shape == tol.shape)
   if(tol.shape=="constant-tolerance"){
@@ -421,7 +473,7 @@ par.plots.complete <- function(dat.var, tol.shape, sub, plot.prev){
   pA <- ggplot(data=r.dat) +
     geom_line(aes(x=variable_par, y=value, color=tolerance)) +
     facet_grid(~label, scales="free", labeller = label_parsed) +
-    ylab(bquote("r"^"*"~", virus growth")) + theme_bw() +
+    ylab(bquote(r[R]^"*"~", virus growth")) + theme_bw() +
     theme(panel.grid = element_blank(), axis.title.x = element_blank(), strip.text = element_text(size=15),
           axis.title.y = element_text(size=18), strip.background = element_rect(fill="white"), axis.text.y = element_text(size=14),
           axis.text.x = element_blank(),axis.ticks.x = element_blank(), plot.margin = unit(c(.5,.1,0,1.3), "cm"),
@@ -433,7 +485,7 @@ par.plots.complete <- function(dat.var, tol.shape, sub, plot.prev){
   pB <- ggplot(data=beta.dat) +
     geom_line(aes(x=variable_par, y=value, color=tolerance), show.legend = F) +
     facet_grid(~label, scales="free", labeller = label_parsed) +
-    ylab(bquote(beta^"*"~", transmission")) + theme_bw() +
+    ylab(bquote(beta[r[R]]^"*"~", transmission")) + theme_bw() +
     theme(panel.grid = element_blank(), axis.title.x = element_blank(), strip.text = element_blank(),
           axis.title.y = element_text(size=18), strip.background = element_blank(), axis.text.y = element_text(size=14),
           axis.text.x = element_blank(),axis.ticks.x = element_blank(), plot.margin = unit(c(0,.1,0,.4), "cm")) + coord_cartesian(ylim = c(0,.005)) +
@@ -445,7 +497,7 @@ par.plots.complete <- function(dat.var, tol.shape, sub, plot.prev){
   pC <- ggplot(data=a.dat) +
     geom_line(aes(x=variable_par, y=value, color=tolerance), show.legend = F) +
     facet_grid(~label, scales="free", labeller = label_parsed) +
-    ylab(bquote(alpha^"*"~", virulence")) + theme_bw() +
+    ylab(bquote(alpha[r[R]]^"*"~", virulence")) + theme_bw() +
     theme(panel.grid = element_blank(), axis.title.x = element_blank(), strip.text = element_blank(),
           axis.title.y = element_text(size=18), strip.background = element_blank(), axis.text = element_text(size=14),
           plot.margin = unit(c(0,.1,1,.7), "cm")) + coord_cartesian(ylim = c(0,.05)) +
@@ -487,7 +539,7 @@ plot.join.complete <- function(dat.var, dat.tol, filename, tol.shape, sub){
          plot = both.plot,
          units="mm",  
          width=170, 
-         height=90, 
+         height=110, 
          scale=3, 
          dpi=300)
 }
@@ -588,7 +640,11 @@ Fig.S3 <- function(dat.var,  filename){
   
 }
 
-Fig.S3(dat.var = subset(out.curves, variable =="Tv_spill" | variable=="Tw_spill"),
+out.spill = subset(out.curves, variable =="Tv_spill" | variable=="Tw_spill")
+out.spill$tolerance_shape <- factor(out.spill$tolerance_shape, levels = c("constant-tolerance", "complete-tolerance"))
+
+
+Fig.S3(dat.var = out.spill,
        filename= paste0(homewd,"supp-figs/FigS3.png"))
 
        
